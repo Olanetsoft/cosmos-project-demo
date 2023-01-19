667c944f246fdd0263d4c54bb7505abf7d723f71
Idris Olubisi <heedris2olubisi@gmail.com>
2023-01-19 01:12:21 +0100

    Ready to ship

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,25 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
-const ethers = require('ethers');
-
-// // This is a sample Hardhat task. To learn how to create your own go to
-// // https://hardhat.org/guides/create-task.html
-// task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-//   const accounts = await hre.ethers.getSigners();
-
-//   for (const account of accounts) {
-//     console.log(account.address);
-//   }
-// });
-
-const mnemonic =
-  "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
-
-  let mnemonicWallet = ethers.Wallet.fromMnemonic(mnemonic);
-
-  // console.log(mnemonicWallet.privateKey);
-
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
@@ -32,12 +13,8 @@ module.exports = {
     },
 
     evmos: {
-      // provider: () =>
-      //   new HDWalletProvider(mnemonic, "https://eth.bd.evmos.dev:8545"),
       url: "https://eth.bd.evmos.dev:8545",
-      accounts:[ mnemonicWallet.privateKey ],
-      // network_id: 9000,
-      // chain_id: 9000,
+      accounts:[process.env.PRIVATE_KEY],
 
     },
   },
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,31 +1,30 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const main = async () => {
+  // Get the contract factory for the "Greeter" contract
+  const greeterContractFactory = await hre.ethers.getContractFactory("Greeter");
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
-
-  const lockedAmount = hre.ethers.utils.parseEther("1");
-
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+  // Deploy the contract to the blockchain
+  const greeterContract = await greeterContractFactory.deploy(
+    "Hello, Hardhat!"
   );
-}
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+  // Wait until the contract is deployed
+  await greeterContract.deployed();
+
+  // Log the address of the deployed contract
+  console.log("Greeter Contract deployed to: ", greeterContract.address);
+};
+
+const runMain = async () => {
+  try {
+    // Call the main function
+    await main();
+    process.exit(0);
+  } catch (error) {
+    // Log any errors that occur
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+// Run the main function
+runMain();
